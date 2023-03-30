7fa879fa54f05859940d541718b1259e63560d90
gerontius <64888786+gerontius@users.noreply.github.com>
2023-03-30 07:05:32 +0100

    initial commit

---
+++ src/Content.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Content = () => {
+    const handleNameChange = () => {
+        const names= ['Bob','Ed','Dave'];
+        const int = Math.floor(Math.random() * 3);
+        return names[int];
+      }
+          
+  return (
+    <main>
+        <p>
+            Hello {handleNameChange()}!
+        </p>
+    </main>
+  )
+}
+
+export default Content
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header style={{
+        backgroundColor: 'mediumorchid',
+        color: '#ccc'
+    }}>
+        <h1>Groceries List</h1></header>
+ )
+}
+
+export default Header
